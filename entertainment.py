10f135b36719681141a7709be61b985da57dc8eb
Ryosuke Honda <honda00888005@gmail.com>
2016-05-22 02:02:36 +0900

    test

--- entertainment.py
+++ entertainment.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import fresh_tomatoes
 import media
 
 toy_story = media.Movie("Toy Story",
@@ -8,6 +8,7 @@ toy_story = media.Movie("Toy Story",
                       "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                       "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-print toy_story.title
 
-toy_story.show_trailer()
+movies=[toy_story,toy_story,toy_story,toy_story,toy_story,toy_story]
+
+fresh_tomatoes.open_movies_page(movies)
