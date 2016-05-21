83a20092c0748bff8459e2298aeb11a5d37b4f66
Ryosuke Honda <honda00888005@gmail.com>
2016-05-22 01:39:13 +0900

    add toy_story

--- entertainment.py
+++ entertainment.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import media
+
+toy_story = media.Movie("Toy Story",
+                      "A boy called Andy Davis (voice: ) uses his toys to act out a bank robbery.",
+                      "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                      "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print toy_story.title
